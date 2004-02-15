55229b180db9025a23bec37bd198e1b483b67eea
Christian Lackas <lackas@invicro.com>
2004-02-15 00:55:02 +0000

    - using Benchmark::cmpthese

--- tools/md5-bench.pl
+++ tools/md5-bench.pl
@@ -4,12 +4,12 @@
 # compares Digest::Perl::MD5 and Digest::MD5
 
 use strict;
-use Benchmark;
+use Benchmark 'cmpthese';
 use Digest::MD5;
 use lib '../lib';
 use Digest::Perl::MD5;
 
-timethese(-60,{
+cmpthese(-60,{
 	'MD5' => 'Digest::MD5::md5(q!delta!)',
 	'Perl::MD5' => 'Digest::Perl::MD5::md5(q!delta!)',
 });
